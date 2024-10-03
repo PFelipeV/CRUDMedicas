da2e8cc2a68a3da3069186bda1dd6596902a71f3
Felipe Vergara <felgara9799@outlook.es>
2024-10-02 19:09:25 -0500

    Primer commit

---
+++ APICitas/Models/MvccrudContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICitas.Models;
+
+public partial class MvccrudContext : DbContext
+{
+    public MvccrudContext()
+    {
+    }
+
+    public MvccrudContext(DbContextOptions<MvccrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC07ADCB2810");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
